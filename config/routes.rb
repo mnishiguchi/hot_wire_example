042b8962885bd8b9464c202820425245e5fa0d6c
Masatoshi N <nishiguchi.masa@gmail.com>
2021-11-16 17:50:40 -0500

    Basic UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    resources :messages, only: %i[new create]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
