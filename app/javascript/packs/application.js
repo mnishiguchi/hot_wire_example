a5e287287e09253f88b79aad4ea0b3a0e3bcb370
Masatoshi N <nishiguchi.masa@gmail.com>
2021-11-23 09:26:25 -0500

    Configure webpacker for importing styles and install Shoelace style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,17 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
+import "stylesheets/main.scss"
+
+// Shoelace components
+// https://shoelace.style/tutorials/integrating-with-rails
+import { setBasePath, defineCustomElements } from '@shoelace-style/shoelace'
+const rootUrl = document.currentScript.src.replace(/\/packs.*$/, '')
+
+// Path to the assets folder (should be independent from the current script source path
+// to work correctly in different environments)
+setBasePath(rootUrl + '/packs/js/')
+
+// This will import all shoelace web components for convenience.
+// https://shoelace.style/getting-started/installation?id=using-webpack
+defineCustomElements()
